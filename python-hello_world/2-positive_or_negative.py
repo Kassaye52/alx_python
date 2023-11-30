90abbfdf9a6cc75555d5a66da29ef095abaf9220
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2023-11-30 10:54:23 -0500

    Programming is like building a multilingual puzzle

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    print(f"{number} is zero")
+else:
+    print(f"{number} is negative")
 
-result = "{} is {}".format(number, "positive" if number > 0 else "zero" if number == 0 else "negative")
-print(result)
