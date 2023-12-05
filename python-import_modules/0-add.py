c71e1a918a6e6d5857af4f3de4c75c404e7eb4b7
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2023-12-05 17:58:27 -0500

    0. Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,5 +3,6 @@ from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    sum = add(a, b)
-    print("The Sum of the two numbers is: {}".format(sum))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
