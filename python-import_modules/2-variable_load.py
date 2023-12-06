36ba8d11a784662314cec232f8e0feaf32ec0dc6
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2023-12-05 21:09:56 -0500

    2. Everything can be imported

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+# Save this as a separate file, for example, main.py
 from variable_load_2 import a
 
-print(a)
+# Define the main behavior of your script
+def main():
+    # Print the value of the imported variable
+    print(a)
+
+# Make sure the script doesn't run when imported
+if __name__ == "__main__":
+    main()
