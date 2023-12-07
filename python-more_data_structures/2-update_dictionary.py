f236e8e16831aa913c7fb99a84836848e41026c6
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2023-12-07 10:45:47 -0500

    2. Update dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,4 +1,2 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    print(a_dictionary)
-    # This function modifies the dictionary in place and does not return anything
