03e91a321f26ba1a55acdd9ea1ee5233a6e50369
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2023-12-07 14:20:24 -0500

    2. Update dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,2 +1,12 @@
 def update_dictionary(a_dictionary, key, value):
+    """
+    Update or add a key-value pair in a dictionary.
+
+    :param a_dictionary: dict - The dictionary to be updated.
+    :param key: str - The key to update or add.
+    :param value: Any - The value to be associated with the key.
+    """
+    # Update the dictionary. This will either replace the value for an existing key,
+    # or add the key-value pair if the key does not exist.
     a_dictionary[key] = value
+
