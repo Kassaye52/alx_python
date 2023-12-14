38799c3a5b4eb2cf41f732f223ba9499caf92a02
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2023-12-14 15:20:43 -0500

    4. Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -3,7 +3,13 @@
 This is a documentation for 4. Improve Geometry
 """
 class BaseGeometry:
+    """
+    BaseGeometry class for handling geometrical operations.
+    """
+    
     def area(self):
-        """Raises an Exception with a message."""
+        """
+        Raises an Exception with a message if the method is not implemented.
+        """
         raise Exception("area() is not implemented")
 
