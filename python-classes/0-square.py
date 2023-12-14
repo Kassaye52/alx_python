3341a2a12cc321d98bbcc7b9919cbf404786c04e
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2023-12-14 10:23:17 -0500

    0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,6 @@
+#!/usr/bin/python3
 class Square:
     """Class that defines a square with a private size attribute."""
-    
     def __init__(self, size):
-        """Initialize the square with a private size attribute."""
-        self.__size = size
-
-    def get_size(self):
-        """Public method to safely access the private size attribute."""
-        return self.__size
+        """Initialize the square with the given size."""
+        self.__size = size  # Private instance attribute
