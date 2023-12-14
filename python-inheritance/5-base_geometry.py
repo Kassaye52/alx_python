2cfadff7299f24b125e4f3c12c11c996a5b3b8de
Kassaye Belay <kassayebelay@Kassayes-MacBook-Pro.local>
2023-12-14 15:14:15 -0500

    Square

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,3 +1,8 @@
+#!/usr/bin/python3
+"""
+This is a documentation for 6. Rectangle
+"""
+
 class BaseGeometry:
     """BaseGeometry class for handling geometrical operations and validation."""
 
@@ -20,40 +25,3 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """Rectangle class inherited from BaseGeometry."""
-
-    def __init__(self, width, height):
-        """Initialize the rectangle."""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Return the area of the rectangle."""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Return the rectangle description."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-
-class Square(Rectangle):
-    """Square class that inherits from Rectangle."""
-
-    def __init__(self, size):
-        """Initialize the square with size as both the width and height."""
-        super().__init__(size, size)
-
-    def area(self):
-        """Return the area of the square."""
-        return self._Rectangle__width * self._Rectangle__height  # Accessing the private attribute from the parent class
-
-    def __str__(self):
-        """Return the square description."""
-        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__width)
-
-
--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -2,40 +2,62 @@
 """
 This is a documentation for 8. Square 
 """
+class BaseGeometry:
+    """BaseGeometry class for handling geometrical operations and validation."""
+
+    def area(self):
+        """Raise an Exception with the message for unimplemented method."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate the integer value.
+
+        Args:
+        name (str): The name of the variable.
+        value (int): The value associated with the name.
+
+        Raises:
+        TypeError: If value is not an integer.
+        ValueError: If value is less or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
 class Rectangle(BaseGeometry):
     """Rectangle class inherited from BaseGeometry."""
-"""
-This is a documentation for 8. Square 
-"""
+
     def __init__(self, width, height):
         """Initialize the rectangle."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    """
-    This is a documentation for 8. Square 
-    """
+
     def area(self):
         """Return the area of the rectangle."""
         return self.__width * self.__height
-    """
-        This is a documentation for 8. Square 
-        """   
+
     def __str__(self):
         """Return the rectangle description."""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
-    """
-    This is a documentation for 8. Square 
-    """   
+
+
 class Square(Rectangle):
     """Square class that inherits from Rectangle."""
-    """
-    This is a documentation for 8. Square 
-    """
+
     def __init__(self, size):
         """Initialize the square with size as both the width and height."""
-        super().__init__(size, size)  
-        self.__size = size  
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self._Rectangle__width * self._Rectangle__height  # Accessing the private attribute from the parent class
+
+    def __str__(self):
+        """Return the square description."""
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__width)
 
 
